ea8f3cf1593f504d2d2ada363b489a6738f5ed56
thomasyang1984 <132956456+thomasyang1984@users.noreply.github.com>
2023-05-09 06:42:40 +0800

    Update compliancealliance
    1st change.
    only change the root and domain

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'a80bf956337e4a12898752e09d3ee3f1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://battle-system-a34.notion.site/notion-a80bf956337e4a12898752e09d3ee3f1',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
