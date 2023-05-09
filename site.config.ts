21157dd944f4340cf39ae7a7787772d0128e9129
thomasyang1984 <132956456+thomasyang1984@users.noreply.github.com>
2023-05-09 10:26:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -21,7 +21,7 @@ export default siteConfig({
   github: 'thomas_yang_1984',
   linkedin: 'thomas_yang_1984',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
+  newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
